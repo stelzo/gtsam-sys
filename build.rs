7e51a37580324eb2aaae8211b3500a448f2bfb09
stelzo <stelzo@steado.de>
2025-06-04 16:11:13 +0200

    compat

--- build.rs
+++ build.rs
@@ -1,6 +1,25 @@
 extern crate bindgen;
+extern crate pkg_config;
 
 use std::{env, path::PathBuf};
+
+/// Tries to use system gtsam and emits necessary build script instructions.
+/// Credits to https://github.com/rust-lang/git2-rs/blob/master/libgit2-sys/build.rs
+fn try_system_gtsam() -> Result<pkg_config::Library, pkg_config::Error> {
+    let mut cfg = pkg_config::Config::new();
+    match cfg.exactly_version("4.2").probe("gtsam") {
+        Ok(lib) => {
+            for include in &lib.include_paths {
+                println!("cargo:root={}", include.display());
+            }
+            Ok(lib)
+        }
+        Err(e) => {
+            // println!("cargo:warning=failed to probe system gtsam: {e}");
+            Err(e)
+        }
+    }
+}
 
 fn main() {
     println!("cargo:rerun-if-changed=gtsam/gtsam_imu.cpp");
@@ -20,12 +39,18 @@ fn main() {
 
     cmake::build("gtsam");
 
+    match try_system_gtsam() {
+        Ok(_) => {}
+        Err(_) => {
+            println!(
+                "cargo:rustc-link-search=native={}/build/gtsam/gtsam",
+                out_path.display()
+            );
+        }
+    }
+
     println!(
         "cargo:rustc-link-search=native={}/build",
-        out_path.display()
-    );
-    println!(
-        "cargo:rustc-link-search=native={}/build/gtsam/gtsam",
         out_path.display()
     );
     println!("cargo:rustc-link-lib=static=gtsam_imu");
